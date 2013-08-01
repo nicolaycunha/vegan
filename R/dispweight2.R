0070a17f6e7992ba031e2d840f8f9685ec585da5
Jari Oksanen <jari.oksanen@oulu.fi>
2013-08-01 14:57:17 +0300

    Veganify Szöcs's dispweight
    We reuse vegan functions and vectorize everything. Now simulation
    is made with simulate.nullmodel(..., "c0_ind"). This is now much
    slower than the original code, because c0_ind is so slow (must
    see if this can be fixed).

---
+++ R/dispweight2.R
@@ -0,0 +1,50 @@
+### Veganified version of Eduard Szöcs's dispweight. This version
+### reuses other vegan utilities as much as possible and follows vegan
+### conventions. Basically, we primarily want to get transformed data
+### with information on transformation. Only basically needed
+### information are the overdispersion coefficients (D) and p-values;
+### the weights can be found from these.
+`dispweight2` <-
+    function(comm, groups, nperm = 999, ...)
+{
+    ## vegan uses 'groups' (not 'group') in other similar
+    ## functions. First remove empty levels
+    groups <- factor(groups)
+    ## Statistic is the sum of squared differences by 'groups'
+    means <- apply(comm, 2, function(x) tapply(x, groups, mean))
+    fitted <- means[groups,]
+    dhat <- colSums((comm - fitted)^2/fitted, na.rm = TRUE)
+    ## Get df for non-zero blocks of species. Completely ignoring
+    ## all-zero blocks for species sounds strange to me, but this was
+    ## done in the original code, and we follow here. However, this
+    ## was not done for significance tests, and only concerns 'D' and
+    ## 'weights'.
+    nreps <- table(groups)
+    div <- colSums(sweep(means > 0, 1, nreps - 1, "*"))
+    ## "significance" of overdispersion is assessed from Chi-square
+    ## evaluated separately for each species. This means fixing only
+    ## marginal totals for species but letting row marginals vary
+    ## freely, unlike in standard Chi-square where both margins are
+    ## fixed. In vegan this is achieved by nullmodel 'c0_ind'. Instead
+    ## of one overall simulation, nullmodel is generated separately
+    ## for each of 'groups'
+    chisq <- function(x) {
+        fitted <- colMeans(x)
+        colSums(sweep(x, 2, fitted)^2, na.rm = TRUE) / fitted
+    }
+    simulated <- matrix(0, nrow = ncol(comm), ncol = nperm)
+    for (lev in levels(groups)) {
+        nm <- nullmodel(comm[groups == lev,], "c0_ind")
+        tmp <- apply(simulate(nm, nperm), 3, chisq)
+        ok <- !is.na(tmp)
+        simulated[ok] <- simulated[ok] + tmp[ok] 
+    }
+    p <- (rowSums(dhat <= simulated) + 1) / (nperm + 1)
+    dhat <- dhat / div
+    w <- ifelse(p <= 0.05, 1/dhat, 1)
+    comm <- sweep(comm, 2, w, "*")
+    out <- list(D = dhat, p = p, weights = w, transformed = comm)
+    class(out) <- "dispweight"
+    out
+}
+
